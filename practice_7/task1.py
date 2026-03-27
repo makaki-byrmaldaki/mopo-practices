683a0ec6ec432f0cc645d50792b5ae6ec0b59ae5
makaki-byrnaldaki <makaki-byrnaldaki@yandex.ru>
2026-03-27 10:35:50 +0500

    Add practice 7 solution

---
+++ practice_7/task1.py
@@ -0,0 +1,5 @@
+count = int(input("Число участников: \n>>> "))
+for i in range(count):
+    name = input("Введите имя: \n>>> ")
+    print(f"Добро пожаловать, {name}")
+print("Групповой чат создан!")
